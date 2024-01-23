cb8825d08625c2d4135142adab742e08a0180576
tiemyz <rm95145@fiap.com.br>
2024-01-23 14:28:53 -0300

    projeto criado

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import GlobalStyles from "./GlobalStyles";
+
+function App() {
+  return (
+    <>
+      <GlobalStyles/>
+      Hello
+    </>
+  )
+}
+
+export default App;
---
+++ src/GlobalStyles.jsx
@@ -0,0 +1,11 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
+    body {
+        margin: 0;
+        padding: 0;
+        font-family: 'Roboto', sans-serif;
+    }
+`;
+
+export default GlobalStyles;

0ce84a95042fd86172df5dedf2704e3b7e367b6b
tiemyz <rm95145@fiap.com.br>
2024-01-24 18:15:20 -0300

    ✨feat: Adição dos elementos do formulário + Logo do Google

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,7 +1,42 @@
+import GoogleLogoImg from '../../assets/logo-googleg-48dp.svg';
+import { LoginContainer, ElementDivs, LoginTitle, GoogleButton, GoogleButtonLogo, GoogleButtonText, FormTitleContainer, FormTitle, FormContainer, StyledForm, Input, Button, LinkCadastro } from './LoginForm.styles';
+
 function LoginForm() {
 
+    /* teste de botão */
+    const handleClick = () => {
+        console.log("este botão está funcionando!");
+      };
+
     return(
-        <h1>Formulário de Login</h1>
+        <LoginContainer>
+            {/* div do titulo */}
+            <ElementDivs >
+                <LoginTitle>Entre no Orange Portfólio</LoginTitle>
+            </ElementDivs>
+
+            {/* div do botão do google */}
+            <ElementDivs>
+                <GoogleButton type="submit" onClick={handleClick}>
+                    <GoogleButtonLogo src={GoogleLogoImg} alt="Google Logo" />
+                    <GoogleButtonText>Entrar com Google</GoogleButtonText>
+                </GoogleButton>
+            </ElementDivs>
+
+            {/* div do formulário */}
+            <FormContainer>
+                <FormTitleContainer>
+                    <FormTitle>Faça login com email</FormTitle>
+                </FormTitleContainer>
+
+                <StyledForm>
+                    <Input type="email" placeholder="Email address"/>
+                    <Input type="password" placeholder="Password"/>
+                    <Button type="submit">ENTRAR</Button>
+                </StyledForm>
+                <LinkCadastro to="/cadastro">Cadastre-se</LinkCadastro>
+            </FormContainer>
+        </LoginContainer>
     );
 }
 

666c9a6e4d8ee1a1b0c724204bb66d92c4fd549f
GrazielaFelix <grazielafelix.dev@gmail.com>
2024-01-25 16:35:25 -0300

    :lipstick: feat: estilizar div do modal com styled components

---
+++ src/components/Modal/ModalStyle.jsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const ModalDiv = styled.div`
+    display: inline-flex;
+    width: 312px;
+    height: 242px;
+    background-color: #FCFDFF;
+    align-items: center;
+    justify-content: center;
+
+`;
+

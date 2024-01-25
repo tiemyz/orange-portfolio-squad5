79ca9abf800aa714dd3ae6353a9b3a1894178588
vlruiz108 <vlruiz@hotmail.com>
2024-01-25 13:28:37 -0300

    chore: adicao de logica com o banco para login

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,8 +1,74 @@
-function LoginForm() {
+import React, { useState, useEffect } from "react";
+import { useSignInWithEmailAndPassword } from "react-firebase-hooks/auth";
+import { Link, useNavigate } from "react-router-dom";
+import { getAuth, signInWithEmailAndPassword, onAuthStateChanged } from "firebase/auth";
+import arrowImg from "../../assets/arrow.svg";
 
-    return(
-        <h1>Formulário de Login</h1>
-    );
+function LoginForm() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [warning, setWarning] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const authInstance = getAuth();
+  const [signIn] = useSignInWithEmailAndPassword(authInstance);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(authInstance, (user) => {
+      if (user) {
+        // redirecionar apenas se o usuário estiver autenticado e não houver avisos
+        if (!warning) {
+          navigate("/");
+        }
+      }
+    });
+
+    return () => unsubscribe();
+  }, [authInstance, navigate, warning]);
+
+  async function handleSignIn(e) {
+    e.preventDefault();
+
+    if (!email || !password) {
+      setWarning("Preencha todos os campos obrigatórios!");
+      return;
+    }
+
+    if (!email.includes("@")) {
+      setWarning("Digite um email válido.");
+      return;
+    }
+
+    setLoading(true);
+
+    try {
+      await signIn(email, password);
+      // limpar os campos se o login for bem sucedido
+      setEmail("");
+      setPassword("");
+    } catch (error) {
+      console.error("Erro ao fazer login:", error.message);
+
+      // Tratamento de erros específicos
+      if (error.code === "auth/wrong-password" || error.code === "auth/user-not-found") {
+        setWarning("Credenciais inválidas. Verifique seu email e senha.");
+      } else if (error.code === "auth/invalid-email") {
+        setWarning("Email incorreto. Por favor, verifique o email digitado.");
+      } else {
+        // Qualquer outro erro na aplicação
+        setWarning("Erro ao fazer login. Por favor, tente novamente.");
+      }
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  return (
+    <div>
+      <h1>Formulário de Login</h1>
+    </div>
+  );
 }
 
 export default LoginForm;

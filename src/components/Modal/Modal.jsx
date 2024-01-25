a205f6b31ecdfe05911aa60cb34cb31b04f9f531
GrazielaFelix <grazielafelix.dev@gmail.com>
2024-01-25 16:34:05 -0300

    :sparkles: feat: adicionar parametros a função modal

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,10 +1,12 @@
-function Modal() {
+import { ModalDiv} from "./ModalStyle";
+
+function Modal({texth5, textbutton}) {
     return(
-        <div>
-            <h5>Projeto Adicionado Com Sucesso</h5>
-            <img src="src\assets\img\check-green.png" alt="check-green"></img>
-            <button>voltar para projetos </button>
-        </div>
+        <ModalDiv>
+            <h5>{texth5}</h5>
+            <img src="src\assets\img\check-green.png" alt="check-green" width={40}></img>
+            <button>{textbutton}</button>
+        </ModalDiv>
     );
 }
 

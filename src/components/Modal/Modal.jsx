91bac006f260b7ea2e0ee92c40fb70b012e19849
GrazielaFelix <grazielafelix.dev@gmail.com>
2024-01-25 15:03:46 -0300

    :sparkles: feat: criar modal

---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,11 @@
+function Modal() {
+    return(
+        <div>
+            <h5>Projeto Adicionado Com Sucesso</h5>
+            <img src="src\assets\img\check-green.png" alt="check-green"></img>
+            <button>voltar para projetos </button>
+        </div>
+    );
+}
+
+export default Modal;

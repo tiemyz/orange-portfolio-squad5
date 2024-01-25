83df6c2cb187c07f0f855502f8d1d6a4977062f0
GrazielaFelix <grazielafelix.dev@gmail.com>
2024-01-25 16:59:20 -0300

    :lipstick: feat: centralizar em coluna elementos do modal

--- src/components/Modal/ModalStyle.jsx
+++ src/components/Modal/ModalStyle.jsx
@@ -2,6 +2,8 @@ import styled from "styled-components";
 
 export const ModalDiv = styled.div`
     display: inline-flex;
+    flex-direction: column;
+    row-gap: 24px;
     width: 312px;
     height: 242px;
     background-color: #FCFDFF;
@@ -12,6 +14,7 @@ export const ModalDiv = styled.div`
 
 export const ModalH5 = styled.h5`
     color: #515255;
+    text-align: center;
     font-style: normal;
     font-size: 24px;
     font-style: normal;

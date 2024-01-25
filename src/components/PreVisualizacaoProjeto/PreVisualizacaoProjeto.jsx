a0b288f7223f4bbfd29aecd6001d63fca27c8829
GrazielaFelix <grazielafelix.dev@gmail.com>
2024-01-25 19:31:41 -0300

    :sparkles: feat: criar estrutura html da pré-visualização do projeto

---
+++ src/components/PreVisualizacaoProjeto/PreVisualizacaoProjeto.jsx
@@ -0,0 +1,24 @@
+function PreVisualizacaoProjeto() {
+    return(
+        <div>
+            <div>
+                <h5>Ecomerce One Page</h5>
+                <img src="" alt="Project-Photo"></img>
+                <div>
+                    <img src="" alt="user-profile" />
+                    <div>
+                        <h6>Nome</h6>
+                        <p></p>
+                        <time></time>
+                    </div>
+                        
+                </div>
+                <p>Teste</p>
+                <h6>Download</h6>
+                <a href="">link</a>
+            </div>
+        </div>
+    );
+}
+
+export default PreVisualizacaoProjeto;

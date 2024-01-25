d95efe51f3d2486be2fa3bb78c105b080b0d5261
GrazielaFelix <grazielafelix.dev@gmail.com>
2024-01-25 20:50:11 -0300

    :sparkles: feat: adicionar parametro isOpen no modal

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,15 +1,21 @@
 import { ButtonOrangeMedium } from "../Buttons/ButtonStyles";
-import { ModalDiv, ModalH5} from "./ModalStyle";
+import { ModalBackgroung, ModalContent, ModalH5} from "./ModalStyle";
 
 
-function Modal({texth5, textbutton}) {
-    return(
-        <ModalDiv>
-            <ModalH5>{texth5}</ModalH5>
-            <img src="src\assets\img\check-green.png" alt="check-green" width={40}></img>
-            <ButtonOrangeMedium>{textbutton}</ButtonOrangeMedium>
-        </ModalDiv>
-    );
+function Modal({isOpen, texth5, textbutton}) {
+    if(isOpen){
+        return(
+            <ModalBackgroung>
+                <ModalContent aria-aria-labelledby="dialog">
+                    <ModalH5>{texth5}</ModalH5>
+                    <img src="src\assets\img\check-green.png" alt="check-green" width={40}></img>
+                    <ButtonOrangeMedium>{textbutton}</ButtonOrangeMedium>
+                </ModalContent>
+            </ModalBackgroung>
+            
+        );
+    }
+    
 }
 
 export default Modal;

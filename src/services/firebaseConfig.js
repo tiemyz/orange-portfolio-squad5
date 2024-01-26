be1e9a60fe046e284d60354e71c693cf59d7ae1e
vlruiz108 <vlruiz@hotmail.com>
2024-01-26 19:03:50 -0300

    chore: realizado importe createUserWithEmailAndPassword

--- src/services/firebaseConfig.js
+++ src/services/firebaseConfig.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { getAuth } from "firebase/auth";
+import { getAuth, createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCy9wJB83lwjoRAXXlkQtYusUwzvnBXv04",
@@ -15,6 +15,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const analytics = getAnalytics(app);
-export { auth, analytics };
+export const auth = getAuth(app);
+export { createUserWithEmailAndPassword, updateProfile };
+//const analytics = getAnalytics(app);

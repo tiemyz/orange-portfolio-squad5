6b965aab17e283827e5ab5aa2f91d9c4a336518a
GrazielaFelix <grazielafelix.dev@gmail.com>
2024-01-26 17:48:09 -0300

    :lipstick: feat: estilizar background e formato do modal

--- src/components/ModalPreVisualizacaoProjeto/ModalPreVisualizacaoProjeto.jsx
+++ src/components/ModalPreVisualizacaoProjeto/ModalPreVisualizacaoProjeto.jsx
@@ -1,24 +1,35 @@
-function PreVisualizacaoProjeto() {
-    return(
-        <div>
-            <div>
-                <h5>Ecomerce One Page</h5>
+import Chip from "../Chip/Chip";
+import { ChipDiv } from "../Chip/ChipStyle";
+import { ModalPreVisualizacaoBackground, ModalPreVisualizacaoContent, ModalPreVisualizacaoH5, Teste } from "./ModalPreVisualizacaoProjetoStyle";
+
+function ModalPreVisualizacaoProjeto() {
+    return (
+        <ModalPreVisualizacaoBackground>
+            <ModalPreVisualizacaoContent>
+                <button>x</button>
+                <ModalPreVisualizacaoH5>Ecomerce One Page</ModalPreVisualizacaoH5>
                 <img src="" alt="Project-Photo"></img>
                 <div>
-                    <img src="" alt="user-profile" />
+
                     <div>
-                        <h6>Nome</h6>
-                        <p></p>
-                        <time></time>
+                        <img src="" alt="user-profile" />
+                        <div>
+                            <sub>Nome</sub>
+                            <time></time>
+                        </div>
+                        <ChipDiv>
+                            <Chip chiptext={'UX'}></Chip>
+                            <Chip chiptext={'Web'}></Chip>
+                        </ChipDiv>
                     </div>
-                        
+                    <p>Teste</p>
+                    <h6>Download</h6>
+                    <a href="">link</a>
                 </div>
-                <p>Teste</p>
-                <h6>Download</h6>
-                <a href="">link</a>
-            </div>
-        </div>
+            </ModalPreVisualizacaoContent>
+        </ModalPreVisualizacaoBackground>
+
     );
 }
 
-export default PreVisualizacaoProjeto;
+export default ModalPreVisualizacaoProjeto;
---
+++ src/components/ModalPreVisualizacaoProjeto/ModalPreVisualizacaoProjetoStyle.jsx
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+
+
+export const ModalPreVisualizacaoBackground = styled.div`
+    display: inline-flex;
+    position: absolute;
+    background-color: rgba(0, 0, 0, 0.5);
+    width: 100%;
+    height: 100%;
+    
+`;
+
+export const ModalPreVisualizacaoContent = styled.div`
+    display: inline-flex;
+    flex-direction: column;
+    width: 100%;
+    height: 100%;
+    margin-top: 15%;
+    border-radius: var(--3, 24px) var(--3, 24px) var(--none, 0px) var(--none, 0px);
+    background: #FFF;
+    align-items: center;
+    align-content: center;
+
+`;
+

ed6d6624e7a2a03271bc4bba7ba1a7c020601e03
Olga Mendes <olgacsmendes@gmail.com>
2024-01-28 16:40:24 -0300

    💄 feat: Ajustes na versão desk. do modal "Adicionar Projeto"

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import GlobalStyles from "./GlobalStyles";
+//import { AddProjectModal } from "./components/AddProjectModal/Desktop/AddProjectModal";
+//import { MobAddProjectModal } from "./components/AddProjectModal/Mobile/MobAddProjectModal"
 
 function App() {
+
+
+
   return (
     <>
       <GlobalStyles/>
     </>
   )
-}
+  }
 
 export default App;

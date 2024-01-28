943222b34cf84b09696ee943c2296f9ea19d7933
DaianePorcena <daiporcenna@gmail.com>
2024-01-28 10:21:36 -0300

    💄 feat:Estilização CSS do componente de navegação

--- src/components/Header/HeaderStyles.jsx
+++ src/components/Header/HeaderStyles.jsx
@@ -3,38 +3,23 @@ import styled from 'styled-components'
 export const HeaderContent = styled.header`
   width: 100vw;
   height: 4rem;
-  padding: 0.75rem 4rem;
-  color: #fff;
+  padding: 12px 24px;
   background: #113;
-  border-radius: 4px;
   box-shadow: 0px 2px 4px -1px rgba(0, 0, 0, 0.2),
     0px 4px 5px 0px rgba(0, 0, 0, 0.14), 0px 1px 10px 0px rgba(0, 0, 0, 0.12);
+  display: flex;
+  align-items: center;
   box-sizing: border-box;
-
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
 `
 
-export const Navbar = styled.nav`
+export const Nav = styled.div`
+  width: 100%;
+  margin: 0;
   display: flex;
   align-items: center;
-  gap: 1rem;
-  flex: 1 0 0;
-`
-export const MobileIcon = styled.button`
-  cursor: pointer;
-  background: transparent;
-  border: none;
-  padding: 8px;
-`
-export const NavContent = styled.div`
-  display: inline-flex;
-  align-items: center;
-  gap: 99px;
+  justify-content: space-between;
 
-  img .logo {
+  img.logo {
     width: 83px;
     height: 31px;
   }
@@ -52,25 +37,109 @@ export const NavContent = styled.div`
     object-fit: cover;
     clip-path: circle();
   }
-`
-export const NavDropDown = styled.div`
-  position: absolute;
-  top: 100%;
-  left: 0;
-  background: #fff;
-  padding: 10px;
-  border-radius: 4px;
-  box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.1);
-  z-index: 1;
-  display: ${({ isOpen }) => (isOpen ? 'block' : 'none')};
+
+  .notification {
+    cursor: pointer;
+  }
+
+  #menu {
+    display: flex;
+  }
+
+  .menu-open,
+  .menu-close {
+    cursor: pointer;
+    padding: 8px;
+    background: transparent;
+    border: none;
+  }
+
+  .hidden {
+    display: none;
+  }
+
+  .buttons {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .show {
+    left: 0;
+  }
+
+  @media (min-width: 768px) {
+    .mobile {
+      display: none;
+    }
+  }
 `
 
-export const NavLink = styled.a`
-  display: block;
-  color: white;
-  text-decoration: none;
-  padding: 8px;
-  &:hover {
-    background: #777;
+export const Navbar = styled.nav`
+  width: 147px;
+  height: 155px;
+  background: #fff;
+  border-radius: 4px;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  position: fixed;
+  top: 64px;
+  left: -159px;
+
+  #navbar li {
+    list-style: none;
+    padding: 6px 16px;
+    align-items: center;
+    position: relative;
+  }
+
+  #navbar li a {
+    color: rgba(0, 0, 0, 0.87);
+    text-decoration: none;
+    font-family: Roboto;
+    font-size: 16px;
+    font-weight: 400;
+    letter-spacing: 0.15px;
+  }
+
+  .divider {
+    width: 147px;
+    height: 1px;
+    margin: 8px 0;
+    background: rgba(0, 0, 0, 0.12);
+  }
+
+  @media (min-width: 768px) {
+    width: auto;
+    height: 41px;
+    margin-left: 100px;
+    background: none;
+    top: 0;
+    right: 0;
+    position: relative;
+    align-items: center;
+    justify-content: center;
+
+    #navbar {
+      display: flex;
+    }
+
+    #navbar li a {
+      color: #fcfdff;
+      font-family: Roboto;
+      font-size: 20px;
+      font-weight: 500;
+      line-height: 20px; /* 100% */
+      letter-spacing: 0.15px;
+    }
+
+    #navbar .config {
+      display: none;
+    }
+
+    .divider {
+      display: none;
+    }
   }
 `

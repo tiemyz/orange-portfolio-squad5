a67a209c402c1f68a839f3fedba807fb076fc00d
DaianePorcena <daiporcenna@gmail.com>
2024-01-28 10:20:52 -0300

    ✨ feat: Modificação da navegação

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,50 +1,66 @@
-import {
-  HeaderContent,
-  Navbar,
-  MobileIcon,
-  NavContent,
-  NavDropDown,
-  NavLink
-} from './HeaderStyles'
-import IconButton from '../../assets/images/menu.svg'
+import { HeaderContent, Nav, Navbar } from './HeaderStyles'
+import MenuOpen from '../../assets/images/menu-open.svg'
 import Logo from '../../assets/images/Logo-orange.png'
 import IconNotifications from '../../assets/images/notifications.png'
-import PhotoProfile from '../../assets/images/perfil.png'
+import PhotoProfile from '../../assets/images/profile.png'
 import { useState } from 'react'
 
 const Header = () => {
-  const [isOpen, setIsOpen] = useState(false)
-  const toggleMenu = () => {
-    setIsOpen(!isOpen)
+  const [isOpen, setOpen] = useState(false)
+
+  const handleMenuOpen = () => {
+    setOpen(true)
+  }
+
+  const handleMenuClose = () => {
+    setOpen(false)
   }
   return (
     <HeaderContent>
-      <Navbar>
-        <MobileIcon onClick={toggleMenu}>
-          <img src={IconButton} alt="Botaõ menu" />
-        </MobileIcon>
-        <NavContent>
-          <img src={Logo} alt="Logo Oranje" className="logo" />
-
-          <div className="perfil">
-            <div className="img-perfil">
-              <img src={PhotoProfile} alt="Foto de perfil" />
-            </div>
-            <div>
-              <img src={IconNotifications} alt="Botão de notificação" />
-            </div>
+      <Nav>
+        <div id="menu">
+          <div className="buttons mobile">
+            <button
+              className={`menu-open ${isOpen ? 'hidden' : ''}`}
+              onClick={handleMenuOpen}
+            >
+              <img src={MenuOpen} alt="Abrir menu" />
+            </button>
+            <button
+              className={`menu-close ${isOpen ? '' : 'hidden'}`}
+              onClick={handleMenuClose}
+            >
+              <img src={MenuOpen} alt="Abrir menu" />
+            </button>
           </div>
 
-          {isOpen && (
-            <NavDropDown isOpen={isOpen}>
-              <NavLink href="#">Meus Projetos</NavLink>
-              <NavLink href="#">Descobrir</NavLink>
-              <div className="divider"></div>
-              <NavLink href="#">Configurações</NavLink>
-            </NavDropDown>
-          )}
-        </NavContent>
-      </Navbar>
+          <img src={Logo} alt="Logo Oranje" className="logo" />
+        </div>
+        <Navbar className={isOpen ? 'show' : ''}>
+          <ul id="navbar">
+            <li>
+              <a href="#">Meus Projetos</a>
+            </li>
+            <li>
+              <a href="#">Descobrir</a>
+            </li>
+            <div className="divider"></div>
+            <li className="config">
+              <a href="#">Configurações</a>
+            </li>
+          </ul>
+        </Navbar>
+        <div className="perfil">
+          <div className="img-perfil">
+            <img src={PhotoProfile} alt="Foto de perfil" />
+          </div>
+          <img
+            src={IconNotifications}
+            alt="Botão de notificação"
+            className="notification"
+          />
+        </div>
+      </Nav>
     </HeaderContent>
   )
 }

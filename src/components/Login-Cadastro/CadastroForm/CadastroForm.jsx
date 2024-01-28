5b53d21bcc40107f0903af0de6800d84c1785793
vlruiz108 <vlruiz@hotmail.com>
2024-01-28 18:22:23 -0300

    chore: adicao de log para tratamento de cadastro de senha

--- src/components/Login-Cadastro/CadastroForm/CadastroForm.jsx
+++ src/components/Login-Cadastro/CadastroForm/CadastroForm.jsx
@@ -82,6 +82,7 @@ function CadastroForm() {
             navigate("/");
         } catch (error) {
             console.error("Erro ao criar conta:", error.message);
+            console.log("Código de erro Firebase:", error.code);
 
             // Tratar erros específicos
             if (error.code === "auth/email-already-in-use") {
@@ -132,9 +133,9 @@ function CadastroForm() {
                     <InputField type={showPassword ? 'text' : 'password'} value={password} onChange={(e) => setPassword(e.target.value)}/>
                     <VisibilityButton type="button" onClick={handleTogglePassword}>
                         {showPassword ? (
-                            <EyesIcon src={EyeClosedIcon} alt="Hide Password"/>
-                        ) : (
                             <EyesIcon src={EyeOpenIcon} alt="Show Password"/>
+                        ) : (
+                            <EyesIcon src={EyeClosedIcon} alt="Hide Password"/>
                         )}
                     </VisibilityButton>
                 </InputContainer>
@@ -146,14 +147,6 @@ function CadastroForm() {
             {loading && <p>Carregando...</p>}
             {!loading && warning && <p style={{ color: "red" }}>{warning}</p>}
             {!loading && successMessage && <p style={{ color: "green" }}>{successMessage}</p>}
-
-            {/* Adicionar um link para redirecionar para outra página, se necessário */}
-            <Link to="/">Voltar para a página inicial</Link>
-
-            {/* Adicionar o botão de login com Google */}
-            <button type="button" onClick={handleSignInWithGoogle}>
-                Entrar com Google
-            </button>
         </CadastroFormContainer>
     );
 }

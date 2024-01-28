7f6f073d3ce69a0057d8dc303c674075ea687311
GrazielaFelix <grazielafelix.dev@gmail.com>
2024-01-28 15:15:30 -0300

    :sparkles: feat: modificar tags do componente pre-visualização de projeto para as tags estilizadas

--- src/components/ModalPreVisualizacaoProjeto/ModalPreVisualizacaoProjeto.jsx
+++ src/components/ModalPreVisualizacaoProjeto/ModalPreVisualizacaoProjeto.jsx
@@ -1,30 +1,37 @@
 import Chip from "../Chip/Chip";
 import { ChipDiv } from "../Chip/ChipStyle";
-import { ModalPreVisualizacaoBackground, ModalPreVisualizacaoContent, ModalPreVisualizacaoH5} from "./ModalPreVisualizacaoProjetoStyle";
+import { ModalPreVisualizacaoBackground, ModalPreVisualizacaoContent, ModalPreVisualizacaoDiv, ModalPreVisualizacaoH5, ModalPreVisualizacaoInfoUsuario, ModalPreVisualizacaoNomeUsuario, ModalPreVisualizacaoTextContainer, ModalPreVisualizacaoTime } from "./ModalPreVisualizacaoProjetoStyle";
 
-function ModalPreVisualizacaoProjeto() {
+function ModalPreVisualizacaoProjeto(url) {
     return (
         <ModalPreVisualizacaoBackground>
+
             <ModalPreVisualizacaoContent>
                 <button>x</button>
                 <ModalPreVisualizacaoH5>Ecomerce One Page</ModalPreVisualizacaoH5>
-                <img src="" alt="Project-Photo"></img>
+
+                <img src="src\assets\img\img_landingpage.png" alt="Project-Photo"></img>
                 <div>
 
-                    <div>
-                        <img src="" alt="user-profile" />
-                        <div>
-                            <sub>Nome</sub>
-                            <time></time>
-                        </div>
+                    <ModalPreVisualizacaoDiv>
+
+                        <img src="src\assets\img\user-profile.png" alt="user-profile" width={40} height={40} />
+                        <ModalPreVisualizacaoInfoUsuario>
+                            <ModalPreVisualizacaoNomeUsuario>Camila Soares</ModalPreVisualizacaoNomeUsuario>
+                            <ModalPreVisualizacaoTime>12/12</ModalPreVisualizacaoTime>
+                        </ModalPreVisualizacaoInfoUsuario>
                         <ChipDiv>
                             <Chip chiptext={'UX'}></Chip>
                             <Chip chiptext={'Web'}></Chip>
                         </ChipDiv>
-                    </div>
-                    <p>Teste</p>
-                    <h6>Download</h6>
-                    <a href="">link</a>
+                    </ModalPreVisualizacaoDiv>
+                    <ModalPreVisualizacaoTextContainer>
+                        <p>Temos o prazer de compartilhar com vocês uma variação da nosso primeiro recurso gratuito, Monoceros. É um modelo de uma página para mostrar seus produtos. Tentamos redesenhar uma versão mais B2C e minimalista do nosso primeiro template de e-commerce.</p>
+                        <br></br>
+                        <h6>Download</h6>
+                        <a href={url}>link</a>
+                    </ModalPreVisualizacaoTextContainer>
+
                 </div>
             </ModalPreVisualizacaoContent>
         </ModalPreVisualizacaoBackground>

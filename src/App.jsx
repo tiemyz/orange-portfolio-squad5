7523d8c8284a718a0fc1f49e8e652561d1946811
DaianePorcena <daiporcenna@gmail.com>
2024-01-28 10:17:20 -0300

    ✨ feat: Atualização e adição de imagens

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import GlobalStyles from './GlobalStyles'
 import Header from './components/Header'
-// import AppRoutes from "./routes/AppRoutes";
+import AppRoutes from './routes/AppRoutes'
 
 function App() {
   return (
     <>
       <GlobalStyles />
-      {/* <AppRoutes/> */}
+      <AppRoutes />
       <Header />
     </>
   )

e01c079bfbff700a53b62410906053468ad5f279
Olga Mendes <olgacsmendes@gmail.com>
2024-01-29 20:17:24 -0300

    :lipstick: feat: Ajustes na seção "meus projetos" c/ botão editar/excluir

--- src/components/SecaoMeusProjetos/SecaoMeusProjetos.jsx
+++ src/components/SecaoMeusProjetos/SecaoMeusProjetos.jsx
@@ -1,40 +1,62 @@
-import {SecaoProjetos, CampoBuscarTags, SubtituloProjetos, BotaoProjeto, StyledBodyText1, StyledBodyText2} from "./SecaoMeusProjetosStyle"
+import { useState } from 'react';
+import { BotaoEditarOuExcluir } from '../BotãoEditarOuExcluir/BotãoEditarOuExcluir';
+import {
+	SecaoProjetos,
+	CampoBuscarTags,
+	SubtituloProjetos,
+	BotaoProjeto,
+	StyledBodyText1,
+	StyledBodyText2,
+	ListaProjetos,
+  BotaoLapis,
+} from './SecaoMeusProjetosStyle';
 
-/* TODO: 
-* Acrescentar quadros "em branco",
-* Verificar necessidade de efeitos hover e focus no input
-* Remover "adicionar primeiro projeto" depois que o usuário já tiver adicionado algum projeto
-* Acrescentar botãozinho de lápis ao lado de miniatura do projeto, 
-para editar ou excluir (Será outro componente)
- */
+/* TODO: O botão de editar tá saindo do lugar com a mudança de tela, consertar isso.
+ - Adicionar os "espaços em branco" na lista de projetos
+ - Retoques na responsividade (verificar a necessidade de tela intermediária) */
 
-export default function SecaoMeusProjetos () {
-    return (
-        <SecaoProjetos>
-            <div className="projetos">
-                <div>
-                    <SubtituloProjetos>Meus projetos</SubtituloProjetos>
-                </div>
-                <div className="campo-de-busca">
-                <label htmlFor="buscar-tags">
-                    <CampoBuscarTags
-                        type="text"
-                        id="buscar-tags"
-                        placeholder="Buscar tags"
-                    />
-                </label>
-                </div>
-                        <BotaoProjeto>
-                            <img
-                                id="icon-img"
-                                src="../../../assets/collections.png"
-                                alt="Icon imagem"
-                            />
-                            <StyledBodyText1>Adicione seu primeiro projeto</StyledBodyText1>
-                            <StyledBodyText2>Compartilhe seu talento com milhares de pessoas</StyledBodyText2>
-                        </BotaoProjeto>
-            </div>
-        </SecaoProjetos>
-            
-    )
+export default function SecaoMeusProjetos() {
+	const [openEdit, setOpenEdit] = useState(false);
+	return (
+		<>
+			<SecaoProjetos>
+				<div className='projetos'>
+					<div>
+						<SubtituloProjetos>Meus projetos</SubtituloProjetos>
+					</div>
+					<div className='campo-de-busca'>
+						<label htmlFor='buscar-tags'>
+							<CampoBuscarTags
+								type='text'
+								id='buscar-tags'
+								placeholder='Buscar tags'
+							/>
+						</label>
+					</div>
+					<ListaProjetos>
+						<BotaoProjeto>
+							<BotaoLapis
+								id='pencil-icon'
+								src='/assets/pencil.png'
+								alt='Pencil icon'
+								onClick={() => setOpenEdit(!openEdit)}
+							/>
+							<img
+								id='icon-img'
+								src='../../../assets/collections.png'
+								alt='Icon imagem'
+							/>
+							<StyledBodyText1>Adicione seu primeiro projeto</StyledBodyText1>
+							<StyledBodyText2>
+								Compartilhe seu talento com milhares de pessoas
+							</StyledBodyText2>
+						</BotaoProjeto>
+            <li></li>
+						<li></li>
+					</ListaProjetos>
+				</div>
+			</SecaoProjetos>
+			{openEdit && <BotaoEditarOuExcluir />}
+		</>
+	);
 }
--- src/components/SecaoMeusProjetos/SecaoMeusProjetosStyle.jsx
+++ src/components/SecaoMeusProjetos/SecaoMeusProjetosStyle.jsx
@@ -3,8 +3,6 @@ import styled from "styled-components";
 //Estilo da Seção Projetos - Mobile
 export const SecaoProjetos = styled.section`
     margin: 40px 0 27px 0;
-    width: 312px;
-    height: 441px;
     display: flex;
     flex-direction: column;
 
@@ -19,7 +17,7 @@ export const SecaoProjetos = styled.section`
 //Estilo do subtítulo "Meus Projetos" - Mobile and Desktop
 export const SubtituloProjetos = styled.h6 `
     margin: 0 0 16px 24px;
-    color: var(--Neutral-130);
+    color: #0B0C0D;
     font-size: 20px;
     font-weight: 500;
     opacity: 0.6;
@@ -30,16 +28,16 @@ export const CampoBuscarTags = styled.input`
     align-self: stretch;
     margin: 0 24px 24px 24px;
     padding: 10px 12px;
-    border: 1px solid var(--Neutral-100);
+    border: 1px solid #818388;
     border-radius: 4px;
     width: 90%;
     height: 24px;
     &:hover {
-        border: 1px solid var(--Neutral-110);
+        border: 1px solid #000000;
     }
     //Para code review: Focus não está funcionando como deveeria (Cor não muda)!
     &:focus {
-        border: 2px solid var(--Info-100);
+        border: 2px solid #2348B1;
     }
 
 //Desktop
@@ -48,20 +46,32 @@ export const CampoBuscarTags = styled.input`
     }
 `;
 
+//Container projetos
+export const ListaProjetos = styled.ul`
+    list-style: none;
+    display: flex;
+    flex-direction: row;
+    margin: 0;
+    padding: 0;
+
+`
+
+
 //Estilo botão projeto - Mobile
 export const BotaoProjeto = styled.button`
+    position: relative;
     margin: 0 24px;
-    padding: 68px 21px;
-    background-color: var(--Neutral-70);
+    padding: 60px;
+    background-color: #E6E9F2;
     border-radius: 4px;
-    width: 100%;
-    height: 70%;
+    width: 389px;
+    height: 258px;
     border: none;
     cursor: pointer;
-    align-content: center;
+    flex-direction: column;
 
 //Desktop
-    @media (min-width: 768px) {
+    @media (max-width: 768px) {
     width: 53%;
     height: 70%;
     }
@@ -69,7 +79,7 @@ export const BotaoProjeto = styled.button`
 
 //Estilo do p 1 do botão
 export const StyledBodyText1 = styled.p`
-    color: var(--Neutral-120);
+    color: #303133;
     opacity: 0.6;
     font-size: 16px;
     font-weight: 400;
@@ -79,10 +89,69 @@ export const StyledBodyText1 = styled.p`
 
 //Estilo do p 2 do botão
 export const StyledBodyText2 = styled.p`
-    color: var(--Neutral-120);
+    color: #303133;
     opacity: 0.6;
     font-size: 14px;
     font-weight: 400;
     text-align: left;
     margin: 0;
 `;
+
+//Estilo do botão de lápis para dropdown:
+
+export const BotaoLapis = styled.img`
+    position: absolute;
+    top: 10px;
+    right: 12px;
+    width: 25px;
+    height: 25px;
+    padding: 4px;
+    border-radius: 100%;
+    background-color: #FFCC99;
+    border: none;
+    box-shadow: 2px 2px 5px 1px rgba(0, 0, 0, 0.1);
+`;
+
+//Estilo do dropdown:
+export const EditDeleteDropdown = styled.div`
+    position: absolute;
+    top: 214px;
+    right: 806px;
+    width: 180px;
+    height: 55px;
+    padding: 16px 0;
+    border-radius: 8px;
+    background-color: white;
+    box-shadow: 2px 2px 5px 1px rgba(0, 0, 0, 0.1);
+    
+    & ::before {
+    content: '';
+    position: absolute;
+    top: -0.3rem;
+    right: 1.1rem;
+    width: 10px;
+    height: 10px;
+    transform: rotate(45deg);
+    background-color: white;
+    }
+`
+
+export const StyledUl = styled.ul`
+    list-style: none;
+    margin: 0;
+    padding: 0;
+    display: flex;
+    flex-direction: column;
+    gap: 2px;
+`
+
+export const StyledLi = styled.li`
+    display: flex;
+    align-items: center;
+    padding: 4px 0 6px 20px;
+    justify-content: space-between;
+    &:hover {
+        background-color: #FFEECC;
+        cursor: pointer;
+    }
+`

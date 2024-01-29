5e5287f1473a5afda33f20158fcf44cf27f52b17
vlruiz108 <vlruiz@hotmail.com>
2024-01-29 20:39:59 -0300

    chore: alteracoes na logica de como o download precisa funcionar

--- src/services/firebaseStorage.js
+++ src/services/firebaseStorage.js
@@ -1,6 +1,5 @@
 // firebaseStorage.js
-
-import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage";
+import { getStorage, ref, uploadBytes, getDownloadURL, deleteObject } from "firebase/storage";
 import { app } from "./firebaseConfig";
 
 // Obtenha uma instância do serviço Storage
@@ -16,13 +15,44 @@ const uploadImage = async (file, folderName) => {
   // Crie uma referência para o arquivo no Storage
   const storageRef = ref(storage, `${folderName}/${file.name}`);
 
-  // Realize o upload do arquivo
-  await uploadBytes(storageRef, file);
+  try {
+    console.log('Iniciando upload do arquivo:', file.name);
 
-  // Obtenha a URL de download do arquivo
-  const downloadURL = await getDownloadURL(storageRef);
+    // Realize o upload do arquivo
+    await uploadBytes(storageRef, file);
 
-  return downloadURL;
+    console.log('Upload concluído com sucesso!');
+
+    // Obtenha a URL de download do arquivo
+    const downloadURL = await getDownloadURL(storageRef);
+
+    console.log('URL de download do arquivo:', downloadURL);
+
+    return downloadURL;
+  } catch (error) {
+    console.error('Erro durante o upload do arquivo:', error);
+    throw error; // Rejoga o erro para que possa ser capturado pelo código chamador
+  }
 };
 
-export { uploadImage };
+/**
+ * Obtém a URL de download de uma imagem no Firebase Storage.
+ * @param {string} imagePath - O caminho da imagem no Storage.
+ * @returns {Promise<string>} - A URL de download da imagem.
+ */
+const getImageURL = async (imagePath) => {
+  const storageRef = ref(storage, imagePath);
+  return await getDownloadURL(storageRef);
+};
+
+/**
+ * Exclui uma imagem do Firebase Storage.
+ * @param {string} imagePath - O caminho da imagem no Storage.
+ * @returns {Promise<void>}
+ */
+const deleteImage = async (imagePath) => {
+  const storageRef = ref(storage, imagePath);
+  await deleteObject(storageRef);
+};
+
+export { uploadImage, getImageURL, deleteImage };

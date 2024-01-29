1a82eb4c10c29145f2af1f6e025ba04c9704f9e2
vlruiz108 <vlruiz@hotmail.com>
2024-01-29 14:59:40 -0300

    chore: ajuste de rota

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -2,14 +2,16 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import CadastroPage from "../pages/Login-Cadastro/CadastroPage/CadastroPage";
 import LoginPage from "../pages/Login-Cadastro/LoginPage/LoginPage";
 import Home from "../pages/Home/Home";
+import { AddProjectModal } from "../components/AddProjectModal/Desktop/AddProjectModal";
 
 function AppRoutes() {
-    return(
+    return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<LoginPage/>}/>
-                <Route path="/cadastro" element={<CadastroPage/>}/>
-                <Route path="/home" element={<Home/>}/>
+                <Route path="/" element={<LoginPage />} />
+                <Route path="/cadastro" element={<CadastroPage />} />
+                <Route path="/home" element={<Home />} />
+                <Route path="/add-project" element={<AddProjectModal />} />
             </Routes>
         </BrowserRouter>
     );

cd222d7e4141700c9ccce4faf42d5111ac24a466
tiemyz <rm95145@fiap.com.br>
2024-01-29 23:04:50 -0300

    💄style: Ajuste de estilos no SuccessPopup

--- src/components/SuccessPopUp/SuccessPopup.styles.js
+++ src/components/SuccessPopUp/SuccessPopup.styles.js
@@ -10,9 +10,11 @@ export const Alert = styled.div`
     border-radius: 4px;
     background: #2E7D32;
     gap: 12px;
+    margin-bottom: 142px; 
     
     @media screen and (max-width: 768px) {
-        width: 312px;
+        width: 280px;
+        margin-bottom: 56px;
     }
 `
 export const CheckImg = styled.img`

6f36e018bb5b602d262bfbb90ed7a9ee31b6e296
Graziela Felix <grazielafelix.dev@gmail.com>
2024-01-30 20:19:03 -0300

    👌 style: remover importações não utilizadas do App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,7 @@
-import SecaoMeusProjetos from "./components/SecaoMeusProjetos/SecaoMeusProjetos";
 import GlobalStyles from './GlobalStyles';
-import Header from './components/Header';
 import AppRoutes from './routes/AppRoutes';
 
-
 function App() {
-
-
 
   return (
     <>

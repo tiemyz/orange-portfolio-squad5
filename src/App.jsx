c45194b05d2f472596e09991f0601252a530ad56
tiemyz <rm95145@fiap.com.br>
2024-01-31 16:10:18 -0300

    💄feat: Ajuste no App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,13 @@ import GlobalStyles from './GlobalStyles';
 import AppRoutes from './routes/AppRoutes';
 
 function App() {
-
+  
   return (
     <>
       <GlobalStyles />
       <AppRoutes />
     </>
   )
-  }
+}
 
 export default App;

bbb73aec101205649fa312a010fefec5538dbcfc
tiemyz <rm95145@fiap.com.br>
2024-02-01 18:18:34 -0300

    🚀feat: Importação de componentes na Home para testes na tela

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,13 @@
-import Header from '../../components/Header/Header'
+import AddProject from '../../components/AddProjectModal/AddProject';
+//import Header from '../../components/Header/Header';
+import LogoutButton from '../../components/LogoutButton/LogoutButton';
 
 function Home() {
   return (
     <div>
-      <Header />
+      {/** <Header />*/}
+      <AddProject/>
+      <LogoutButton/>
     </div>
   )
 }

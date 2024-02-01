d770a15d6441fa426d42d430fa1d49a0dd934af6
vlruiz108 <vlruiz@hotmail.com>
2024-02-01 16:07:23 -0300

    add: adicao e configuracao do swagger

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -4,6 +4,9 @@ import LoginPage from "../pages/Login-Cadastro/LoginPage/LoginPage";
 import Home from "../pages/Home/Home";
 import { AddProjectModal } from "../components/AddProjectModal/Desktop/AddProjectModal";
 
+// Adicione as importações necessárias para as rotas de documentação
+import SwaggerUI from 'swagger-ui-react';
+
 function AppRoutes() {
     return (
         <BrowserRouter>
@@ -12,6 +15,13 @@ function AppRoutes() {
                 <Route path="/cadastro" element={<CadastroPage />} />
                 <Route path="/home" element={<Home />} />
                 <Route path="/add-project" element={<AddProjectModal />} />
+
+                {/* Adicione a rota de documentação */}
+                <Route
+                    path="/documentation"
+                    element={<SwaggerUI url="/swagger_documentation.json"/>}
+                />
+
             </Routes>
         </BrowserRouter>
     );

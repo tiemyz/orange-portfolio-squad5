4d850a2cfab6ae38257f2102e07e7f97690c7ae6
tiemyz <rm95145@fiap.com.br>
2024-02-01 18:22:21 -0300

    ✨feat: Criação de componente extra para testes relacionados a adição de projetos

---
+++ src/components/AddProjectModal/AddProject.styles.js
@@ -0,0 +1,40 @@
+import styled from "styled-components";
+
+export const AddProjectContainer = styled.div`
+  position: absolute;
+  margin-top: 100px;
+  background-color: gray;
+  padding: 4px;
+  width: 890px;
+  height: 522px;
+`;
+
+export const DivTitulo = styled.div`
+  //background-color: yellow;
+`;
+
+export const DivBotaoForm = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const DivH3Img = styled.div`
+  //background-color: green;
+`;
+
+export const DivForm = styled.div`
+  //background-color: pink;
+`;
+
+export const DivFormInput = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const DivLink = styled.div`
+  //background-color: cyan;
+`;
+
+export const DivBotoes = styled.div`
+  //background-color: orange;
+`;

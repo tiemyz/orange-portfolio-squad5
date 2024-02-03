59dbeef24bc69e189f7c2c5165188600ab1683ad
vlruiz108 <vlruiz@hotmail.com>
2024-02-02 23:22:11 -0300

    chore: adicao de rota para teste

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,26 +1,36 @@
+import React, { useState } from 'react';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import CadastroPage from "../pages/Login-Cadastro/CadastroPage/CadastroPage";
 import LoginPage from "../pages/Login-Cadastro/LoginPage/LoginPage";
 import Home from "../pages/Home/Home";
-
-// Adicione as importações necessárias para as rotas de documentação
-import SwaggerUI from 'swagger-ui-react';
+import Header from "../components/Header/Header";
+import Perfil from "../components/Perfil/Perfil";
+import { ProfileImageContextProvider } from '../components/contex/ProfileImageContext';
 
 function AppRoutes() {
+    const [profileImage, setProfileImage] = useState(null);
+
     return (
         <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<LoginPage />} />
-                <Route path="/cadastro" element={<CadastroPage />} />
-                <Route path="/home" element={<Home />} />
-
-                {/* Adicione a rota de documentação */}
-                <Route
-                    path="/documentation"
-                    element={<SwaggerUI url="/swagger_documentation.json"/>}
-                />
-
-            </Routes>
+            {/* Adicione o ProfileImageContextProvider aqui */}
+            <ProfileImageContextProvider>
+                <Routes>
+                    <Route path="/" element={<LoginPage />} />
+                    <Route path="/cadastro" element={<CadastroPage />} />
+                    <Route
+                        path="/home"
+                        element={<Home setProfileImage={setProfileImage} />}
+                    />
+                    <Route
+                        path="/perfil"
+                        element={<Perfil setProfileImage={setProfileImage} />}
+                    />
+                    <Route
+                        path="/header"
+                        element={<Header profileImage={profileImage} />}
+                    />
+                </Routes>
+            </ProfileImageContextProvider>
         </BrowserRouter>
     );
 }

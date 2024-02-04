dd5f7dae69e951e055c25057c6756de674688640
tiemyz <rm95145@fiap.com.br>
2024-02-04 19:57:57 -0300

    📁chore: Atualização do AppRoutes para o modelo antigo

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -3,34 +3,15 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import CadastroPage from "../pages/Login-Cadastro/CadastroPage/CadastroPage";
 import LoginPage from "../pages/Login-Cadastro/LoginPage/LoginPage";
 import Home from "../pages/Home/Home";
-import Header from "../components/Header/Header";
-import Perfil from "../components/Perfil/Perfil";
-import { ProfileImageContextProvider } from '../components/contex/ProfileImageContext';
 
 function AppRoutes() {
-    const [profileImage, setProfileImage] = useState(null);
-
     return (
         <BrowserRouter>
-            {/* Adicione o ProfileImageContextProvider aqui */}
-            <ProfileImageContextProvider>
-                <Routes>
-                    <Route path="/" element={<LoginPage />} />
-                    <Route path="/cadastro" element={<CadastroPage />} />
-                    <Route
-                        path="/home"
-                        element={<Home setProfileImage={setProfileImage} />}
-                    />
-                    <Route
-                        path="/perfil"
-                        element={<Perfil setProfileImage={setProfileImage} />}
-                    />
-                    <Route
-                        path="/header"
-                        element={<Header profileImage={profileImage} />}
-                    />
-                </Routes>
-            </ProfileImageContextProvider>
+            <Routes>
+                <Route path="/" element={<LoginPage />} />
+                <Route path="/cadastro" element={<CadastroPage />} />
+                <Route path="/home" element={<Home />}/>    
+            </Routes>
         </BrowserRouter>
     );
 }

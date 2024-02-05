13edfa5e6ef3c4e6e31dda57cc610e4a4cd05839
tiemyz <rm95145@fiap.com.br>
2024-02-05 16:14:31 -0300

    ✨feat: Criação do componente EditarPopUp

---
+++ src/components/ModalEditarPopUp/ModalEditarPopUp.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { ButtonOrangeMedium } from '../Buttons/Buttons.styles';
+import { ModalBackground, ModalContent, ModalH5 } from '../Modal/Modal.styles';
+import CheckImg from '../../assets/images/check-green.png';
+
+function ModalEditarPopUp({ onClose }) {
+  return (
+    <ModalBackground>
+      <ModalContent aria-labelledby="dialog">
+        <ModalH5>Edição concluída com sucesso!</ModalH5>
+        <img src={CheckImg} alt="check-green" width={40}></img>
+        <ButtonOrangeMedium onClick={onClose}>
+          Voltar para Projetos
+        </ButtonOrangeMedium>
+      </ModalContent>
+    </ModalBackground>
+  );
+}
+
+export default ModalEditarPopUp;
